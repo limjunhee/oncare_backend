541be7af225ddbb7983759dd404b22962a8e5158
seokam36 <seokam36@gmail.com>
2026-09-23 18:03:59 +0900

    base타임 설정

--- src/main/java/oncare_backend/OncareBackendApplication.java
+++ src/main/java/oncare_backend/OncareBackendApplication.java
@@ -2,8 +2,10 @@ package oncare_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OncareBackendApplication {
 
 	public static void main(String[] args) {
